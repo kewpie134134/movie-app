20e48fd4d294279ea2d626193784c0747977a4df
Baba, Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2025-06-11 11:11:32 +0900

    大枠完成

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MovieDetail from "./MovieDetail.tsx";
+import Header from "./Header.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  { path: "/", Component: App },
+  { path: "movies/:id", Component: MovieDetail },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Header>
+      <RouterProvider router={router} />
+    </Header>
+  </StrictMode>
+);
